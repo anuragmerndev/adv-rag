3933d343ab5977a5ef2ab5ce989b4a0cd7f9331f
anuragmerndev <anuragmerndeveloper@gmail.com>
2026-04-11 17:15:05 +0530

    chore(validators): remove unused deleteDocumentValidator

--- src/validators/rag.validators.ts
+++ src/validators/rag.validators.ts
@@ -11,9 +11,3 @@ export const queryValidator = z.object({
 });
 
 export type QueryInput = z.infer<typeof queryValidator>;
-
-export const deleteDocumentValidator = z.object({
-    id: z
-        .string({ required_error: 'id is required' })
-        .min(1, 'id cannot be empty'),
-});
